df860d5a61834ec09cd5a67ed09233478ba3458c
Rahul <rahulsharmars1854@gmail.com>
2023-01-02 12:06:34 +0530

    node basic

---
+++ app.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+// function rqlistener(req, res){
+
+// }
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+
+        res.write('<html>');
+        res.write('<head><title> Riderct page</title></head>');
+        res.write('<body><form action= "/message" method = "POST"><input type ="text" name ="message"><button type = "submit">SEND</button></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data' , (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+      return  req.on('end' , () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            
+            fs.writeFileSync('message2.txt' , message, (err => {
+                res.statusCode = 302;
+                res.setHeader('Location' , '/');
+                return res.end();
+            }));
+           
+        });
+      
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from server</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+server.listen(3000);
